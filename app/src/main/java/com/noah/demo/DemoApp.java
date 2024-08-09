2964c34c78e7e4011431fb578f097787533079d8
libingcheng <libingcheng@changdou.vip>
2024-08-09 14:05:42 +0800

    1.0.0版本首次提交

---
+++ app/src/main/java/com/noah/demo/DemoApp.java
@@ -0,0 +1,20 @@
+package com.noah.demo;
+
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
+
+public class DemoApp extends MultiDexApplication {
+    @Override
+    protected void attachBaseContext(final Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ app/src/main/java/com/noah/demo/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.noah.demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.aviran.cookiebar2.CookieBar;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Demo");
+        setContentView(R.layout.activity_bidding);
+    }
+}
